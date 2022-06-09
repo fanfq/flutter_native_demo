fd79236f771a5779655fe0f76b2435ae6648f5e8
fred <fangqing.fan@hotmail.com>
2022-06-09 20:53:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_native_demo
+
+[详细教程](https://www.jianshu.com/p/9c1e14fc16ec)
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+
+    callNativeMethod(_counter);
+  }
+
+  // 创建渠道
+  static const MethodChannel channel =  MethodChannel("your_channel_name");
+
+  Future<void> callNativeMethod(int msg) async {
+    try {
+      // 通过渠道，调用原生代码代码的方法
+      _counter = await channel.invokeMethod("your_method_name", {"msg": msg} );
+      // 打印执行的结果
+      setState(() {
+        _counter;
+      });
+      print("result $_counter");
+    } on PlatformException catch(e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
